97201e824b4869198f13d586ff18013bccc7eca9
wjd514 <wjd514@naver.com>
2022-07-09 18:09:17 +0900

    스프링 핵심 원리 이해1 - 예제 만들기

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,26 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        assertThat(member,is(equalTo(findMember)));
+
+    }
+}
