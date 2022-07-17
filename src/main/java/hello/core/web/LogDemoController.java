1c2946d7756bc3ec2904d62bdcf6d34b7858382d
wjd514 <wjd514@naver.com>
2022-07-18 01:29:50 +0900

    빈 스코프 - 웹 스코프

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,7 +2,6 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,13 +14,14 @@ public class LogDemoController {
 
     private  final LogDemoService logDemoService;
     //request 스코프 빈은 request요청이 있을경우에만 생성되므로 애플리케이션 실행시 오류발생
-    //private  final MyLogger myLogger;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    //request 스코프에 프록시 사용시 사용가능
+    private  final MyLogger myLogger;
+    //private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
+        //MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURI().toString();
         myLogger.setRequsetURL(requestURL);
 
